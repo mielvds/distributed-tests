0223f4b54c05683aa5eeb2e7211f4697773a20a8
Laurens De Vocht <laurensdv@gmail.com>
2015-01-07 13:03:59 +0100

    added ref to configS3

--- 0_build/get_data_generic.sh
+++ 0_build/get_data_generic.sh
@@ -8,7 +8,7 @@ then
 	exit -1
 fi
 
-wget https://s3.amazonaws.com/bmtriplesdata/configS3.txt /home/ubuntu/
+cp /home/ubuntu/configuration_scripts_benchmarks/0_build/configS3.txt /home/ubuntu/
 mv /home/ubuntu/configS3.txt /home/ubuntu/.s3cfg 
 sed -i 's/AWS_AK/'$AWS_ACCESS_KEY'/' .s3cfg
 sed -i 's/AWS_SK/'$AWS_SECRET_KEY'/' .s3cfg
