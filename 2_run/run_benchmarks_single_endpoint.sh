19280b0a9a57406ff201ed49957b1b24902c70b7
Laurens De Vocht <laurensdv@gmail.com>
2015-01-14 11:48:32 +0100

    fixed typo in runscript for bms

--- 2_run/run_benchmarks_single_endpoint.sh
+++ 2_run/run_benchmarks_single_endpoint.sh
@@ -2,31 +2,31 @@
 
 cd
 
-mkdir ./output
-mkdir ./output/fedx
-mkdir ./output/sesame
-mkdir ./output/mmlab
+mkdir ~/output
+mkdir ~/output/fedx
+mkdir ~/output/sesame
+mkdir ~/output/mmlab
 
 cd FedBench\ 3.0
-
+chmod +x ./runEval.sh
 
 # FedX
 cp -a ./configuration_scripts_benchmarks/benchmark/single_endpoint/config_fedx ./configuration_scripts_benchmarks/benchmark/config
-./runval.sh
+./runEval.sh
 
 # DO SMTH WITH OUTPUT
 cp -R ./result /home/ubuntu/output/fedx
 
 # Sesame
 cp -a ./configuration_scripts_benchmarks/benchmark/single_endpoint/config_sesame ./configuration_scripts_benchmarks/benchmark/config
-./runval.sh
+./runEval.sh
 
 # DO SMTH WITH OUTPUT
 cp -R ./result /home/ubuntu/output/sesame
 
 # MMLab Distributed SPARQL
 cp -a ./configuration_scripts_benchmarks/benchmark/single_endpoint/config_mmlab ./configuration_scripts_benchmarks/benchmark/config
-./runval.sh
+./runEval.sh
 
 # DO SMTH WITH OUTPUT
 cp -R ./result /home/ubuntu/output/mmlab
