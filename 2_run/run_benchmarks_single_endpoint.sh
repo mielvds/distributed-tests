a124c1ea7578a54b745c8ae2a6bbb4ba16b4487e
Laurens De Vocht <laurensdv@gmail.com>
2015-01-14 11:43:25 +0100

    updated benchmark script

--- 2_run/run_benchmarks_single_endpoint.sh
+++ 2_run/run_benchmarks_single_endpoint.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
 cd
+
+mkdir ./output
+mkdir ./output/fedx
+mkdir ./output/sesame
+mkdir ./output/mmlab
+
 cd FedBench\ 3.0
+
 
 # FedX
 cp -a ./configuration_scripts_benchmarks/benchmark/single_endpoint/config_fedx ./configuration_scripts_benchmarks/benchmark/config
 ./runval.sh
 
 # DO SMTH WITH OUTPUT
+cp -R ./result /home/ubuntu/output/fedx
 
 # Sesame
 cp -a ./configuration_scripts_benchmarks/benchmark/single_endpoint/config_sesame ./configuration_scripts_benchmarks/benchmark/config
 ./runval.sh
 
 # DO SMTH WITH OUTPUT
+cp -R ./result /home/ubuntu/output/sesame
 
 # MMLab Distributed SPARQL
 cp -a ./configuration_scripts_benchmarks/benchmark/single_endpoint/config_mmlab ./configuration_scripts_benchmarks/benchmark/config
 ./runval.sh
 
 # DO SMTH WITH OUTPUT
-
-cp -a ./configuration_scripts_benchmarks/benchmark/single_endpoint/config_mmlab ./configuration_scripts_benchmarks/benchmark/config
-./runval.sh
-
-# DO SMTH WITH OUTPUT
+cp -R ./result /home/ubuntu/output/mmlab
