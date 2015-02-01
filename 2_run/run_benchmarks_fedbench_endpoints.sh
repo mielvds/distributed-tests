a1a35225fef4862c6b764e3bbdb2edb1cc7f757d
Miel Vander Sande <miel.vandersande@ugent.be>
2015-02-01 14:52:24 +0100

    Added final additions for ldf

--- 2_run/run_benchmarks_fedbench_endpoints.sh
+++ 2_run/run_benchmarks_fedbench_endpoints.sh
@@ -16,11 +16,18 @@ cp -af ~/configuration_scripts_benchmarks/benchmark/fedbench_endpoints/config_fe
 # DO SMTH WITH OUTPUT
 cp -R ./result /home/ubuntu/output/fedx
 
+sources=('kegg-chebi' 'dbpedia' 'drugbank' 'geonames' 'linkedmdb' 'nytimes' 'jamendo' 'sp2b' 'swdf');
+
+for i in "${sources[@]}"
+do
+
 # LDF
-cp -af /home/ubuntu/FedBench 3.0/federated-ldf/config-kegg-chebi.json /home/ubuntu/FedBench 3.0/federated-ldf/config-fedbench.json
+cp -af /home/ubuntu/FedBench 3.0/federated-ldf/config-${i}.json /home/ubuntu/FedBench 3.0/federated-ldf/config-fedbench.json
 cp -af ~/configuration_scripts_benchmarks/benchmark/fedbench_endpoints/config_ldf/* ./config/
 ./runEval.sh
 
 # DO SMTH WITH OUTPUT
-mkdir ./result /home/ubuntu/output/ldf/kegg-chebi/
-cp -R ./result /home/ubuntu/output/ldf/kegg-chebi/
+mkdir ./result /home/ubuntu/output/ldf/${i}/
+cp -R ./result /home/ubuntu/output/ldf/${i}/
+
+done
