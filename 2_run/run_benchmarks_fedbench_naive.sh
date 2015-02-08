68bc76aba0e6d9a3cd70627ec40d5cffd23ffe06
Miel Vander Sande <miel.vandersande@ugent.be>
2015-02-08 12:49:28 +0100

    Added naive confi

---
+++ 2_run/run_benchmarks_fedbench_naive.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd
+
+mkdir ~/output
+mkdir ~/output/fedx
+mkdir ~/output/ldf
+
+cd FedBench\ 3.0
+chmod +x ./runEval.sh
+
+# FedX
+#cp -af ~/configuration_scripts_benchmarks/benchmark/fedbench_endpoints/config_fedx/* ./config/
+#./runEval.sh
+
+# DO SMTH WITH OUTPUT
+#cp -R ./result /home/ubuntu/output/fedx
+
+cp -af ~/configuration_scripts_benchmarks/benchmark/fedbench_endpoints/config_ldf/* ./config/
+
+# LDF
+cp -af /home/ubuntu/FedBench\ 3.0/federated-ldf/config-federated-all.json /home/ubuntu/FedBench\ 3.0/federated-ldf/config-fedbench.json
+./runEval.sh
+
+# DO SMTH WITH OUTPUT
+mkdir ./result /home/ubuntu/output/ldf/all/
+cp -R ./result /home/ubuntu/output/ldf/all/
+
+done
